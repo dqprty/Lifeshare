85aa5e24181d707a1a1f41bd16ed4a7432b566ae
dqprty <67037009+dqprty@users.noreply.github.com>
2020-07-14 12:42:27 +0900

    calendar

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,4 @@
 class Message < ApplicationRecord
-  belongs_to :group
   belongs_to :user
   belongs_to :calendar
 
--- db/migrate/20200708025538_create_messages.rb
+++ db/migrate/20200708025538_create_messages.rb
@@ -3,9 +3,8 @@ class CreateMessages < ActiveRecord::Migration[6.0]
     create_table :messages do |t|
       t.string :content
       t.string :image
-      t.references :group, foreign_key: true
-      t.references :user, foreign_key: true
-      t.references :calendar, foreign_key: true
+      t.integer :user_id, foreign_key: true
+      t.integer :calendar_id, foreign_key: true
       t.timestamps
     end
   end
